09744f3e9a70c795fce3785c347f3b984e4c9b21
Alihan-Mahmudov <alihanmahmudov711@gmail.com>
2022-11-11 16:03:31 +0600

    ff

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm')ewaztrxytcuyvgubhinj;omk'p'ijukyvjtchfdgfxg
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
